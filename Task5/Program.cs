28102a2b0b7027dc3ab2f211f3df85cd62c9febc
Ilya Nurullin <nurullinin@mail.ru>
2017-09-25 00:12:24 +0500

    Add user input

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -12,18 +12,29 @@ namespace Task5
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Дана действительная квадратная матрица порядка n. Найти наибольшее из значений элементов, " +
+                              "расположенных в заштрихованной части матрицы:");
             while (true)
             {
                 Console.Write("Введите значение порядка матрицы: ");
-                int size = int.Parse(Console.ReadLine());
+                try
+                {
+                    int size = int.Parse(Console.ReadLine());
+                    if (size < 1 )
+                        throw new FormatException();
 
-                _matrix = new float[size, size];
+                    _matrix = new float[size, size];
 
-                generateRandomMartix();
+                    fillMatrix(size);
 
-                printMatrix();
+                    printMatrix();
 
-                Console.WriteLine(maxElement());
+                    Console.WriteLine("Максимальный элемент в области = {0}", maxElement());
+                }
+                catch (FormatException)
+                {
+                    Console.Write("Порядок матрицы должен быть натуральным числом. Попробуйте ещё раз.");
+                }
             }
         }
 
@@ -67,6 +78,56 @@ namespace Task5
                     Console.Write("{0:0.0000000 }",_matrix[i, j]);
                 }
                 Console.WriteLine();
+            }
+        }
+
+        static void fillMatrix(int size)
+        {
+            string answer = "";
+            while (true)
+            {
+                Console.WriteLine("1. Сгенерировать матрицу случайно");
+                Console.WriteLine("2. Ввести матрицу вручную");
+                Console.Write("Выберите вариант 1 или 2: ");
+                answer = Console.ReadLine();
+                if (answer != "1" && answer != "2")
+                {
+                    Console.WriteLine("Ожидалось 1 или 2");
+                }
+                else
+                    break;
+            }
+            if (answer == "1")
+                generateRandomMartix();
+            else
+            {
+                Console.WriteLine("Вводите матрицу построчно. Элементы каждой строки разделяются пробелом. Тип элементов: float");
+                for (int i = 0; i < size; i++)
+                {
+                    Console.Write("Введите через пробел элементы строки {0}: ", i + 1);
+                    try
+                    {
+                        var elems = Console.ReadLine().Split().Select(float.Parse).ToArray();
+
+                        if (elems.Length != size)
+                        {
+                            Console.WriteLine("Количество элементов не равно порядку матрицы. Порядок матрицы = {0}, " +
+                                              "количетсов введенных элементов = {1}. Введите эту строчку ещё раз", size, elems.Length);
+                            i--;
+                            continue;
+                        }
+                        for (int j = 0; j < size; j++)
+                        {
+                            _matrix[i, j] = elems[j];
+                        }
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Тип элементов должен быть float. Введите эту строчку ещё раз.");
+                        i--;
+                        continue;
+                    }
+                }
             }
         }
     }
